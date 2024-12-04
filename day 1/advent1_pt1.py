5d8f3416f4238dd8b3cb4046a6d7b94097df393b
lolzieslol <e-vilbjo4@asvg.no>
2024-12-04 22:20:47 +0100

    initial, day 1

---
+++ day 1/advent1_pt1.py
@@ -0,0 +1,36 @@
+filpath = '1/real_input.txt'
+fil = open(filpath,encoding='utf-8')
+content = fil.read()
+list1 = []
+list2 = []
+
+lines = content.split('\n')
+
+for line in lines:
+    if len(line) < 4:
+        continue
+    
+    row = line.split('   ')
+    if len(row) != 2:
+        continue
+    
+    list1.append(int(row[0]))
+    list2.append(int(row[1]))
+
+
+rounds = len(lines)
+print(rounds)
+differences = []
+for i in range(rounds):
+    differences.append(abs(min(list1) - min(list2)))
+    list1.remove(min(list1))
+    list2.remove(min(list2))
+    if len(list1) == 0:
+        print(i)
+        break
+
+total_distance = 0
+for distance in differences:
+    total_distance += distance
+
+print(total_distance)
---
+++ day 1/advent1_pt2.py
@@ -0,0 +1,28 @@
+
+filpath = '1/real_input.txt'
+fil = open(filpath,encoding='utf-8')
+content = fil.read()
+list1 = []
+list2 = []
+
+lines = content.split('\n')
+
+for line in lines:
+    if len(line) < 4:
+        continue
+    
+    row = line.split('   ')
+    if len(row) != 2:
+        continue
+    
+    list1.append(int(row[0]))
+    list2.append(int(row[1]))
+
+
+
+# uniques1 = set(list1)
+score = 0
+for number in list1:
+    score += number * list2.count(number) 
+
+print(score)
